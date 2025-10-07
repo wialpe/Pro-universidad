224057eff05afe9dd752a6929edb2e637a6ad143
william <william.altafulla@gmail.com>
2025-10-07 14:29:27 -0500

    pasando componentes

---
+++ src/admin/components/Aside.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Aside() {
+  return (
+        <div id="sidebar-wrapper">
+          <ul className="sidebar-nav">
+            <li className="sidebar-brand">
+              <a href="#">Start Bootstrap</a>
+            </li>
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+            <li>
+              <a href="#">Shortcuts</a>
+            </li>
+            <li>
+              <a href="#">Overview</a>
+            </li>
+          </ul>
+        </div>
+  )
+}
